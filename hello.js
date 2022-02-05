a215318a9ce881b0c2eda7b52fbbc3c0dfc51027
nomandhoni-cs <nomandhoni@hotmail.comm>
2022-02-05 14:52:06 +0600

    Added new array of friends and fruitse

--- hello.js
+++ hello.js
@@ -1,4 +1,34 @@
+//Here I used the Integer Variable
+var bananaPrice = 5;
+//Here I used console.log to print out the bananaPrice 
+console.log("The price of a banana is $" + bananaPrice);
+//Here I used console.log to print out the bananaPrice variable type
+console.log("The type of bananaPrice is " + typeof bananaPrice);
+
+//Here under " " to declare a variable that is a string
 var yourName = "Noman";
-console.log(yourName)
+//Here i used TypeScript to print out the variable yourName
+console.log(`Your name is${yourName}`);
 var sayHello = "Hello";
 console.log(sayHello + " " + yourName);
+//Here I used typeof to check the type of the variable type
+console.log(typeof(sayHello));
+
+//Array
+var friendsName = ["Noman", "Sabbir", "Mridul", "Faysal"];
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+//Here I used console.log to print out the fruits type
+console.log(typeof(fruits));
+//Here I used console.log to print out the fruits array
+console.log(fruits[3]);
+
+//Here comes the fun part
+console.log(yourName + " Loves to eat " + fruits[3]);
+
+//Also I can count the number of elements in the array
+console.log(fruits.length);
+
+//Here i used for loop  to print the array with name of friends
+for (var i=0; i<friendsName.length; i++){
+    console.log(friendsName[i] + " Loves to eat " + fruits[i]);
+}
