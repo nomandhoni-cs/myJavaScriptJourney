07fb1b9180046c9afecb93aed7e4380adb990270
nomandhoni-cs <nomandhoni@hotmail.comm>
2022-02-18 12:10:36 +0600

    Reorganized filea and folders and added inch to feet converter and const let

---
+++ applyJavaScriptConcepts/constLet.js
@@ -0,0 +1,10 @@
+//This file is used to show the difference between const and let
+// const is used to define a constant variable
+const pi = 3.14;
+//You can't change the value of a constant variable
+let radius = 10;
+//But you can change the value of a let variable
+radius = 20;
+//Here I used console.log to print out the radius
+console.log(radius);
+console.log(pi);
---
+++ applyJavaScriptConcepts/inchToFeet.js
@@ -0,0 +1,12 @@
+// //In this module all about applying JavaScript concepts
+function inchToFeet(inch) {
+    var feet = inch / 12;
+//Here I used .toFixed(3) to round the number
+    feet = feet.toFixed(3);
+    return feet;
+}
+console.log("Length of area " + inchToFeet(300) + " Feet");
+console.log("Leangth of Bed " + inchToFeet(27) + " Feet");
+// var inch = 156;
+// var feet = inch/12;
+// console.log(feet)
