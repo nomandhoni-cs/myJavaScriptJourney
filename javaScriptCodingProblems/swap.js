c5caec1631f13b7be8b48a1d45c151173647c3f4
nomandhoni-cs <nomandhoni@yahoo.com>
2022-03-06 16:21:01 +0600

    Added new directory called 'javaScript Coding Problem' and created swap.js added 3 type of swap

---
+++ javaScriptCodingProblems/swap.js
@@ -0,0 +1,28 @@
+//In this code below I explained 3 type of swap in JavaScript
+
+//First type of Swap
+var a = 7;
+var b = 5;
+console.log("Before swap: a = ",a, "b = ",b);
+var temp = a;
+a = b;
+b = temp;
+console.log("After swap: a = ",a, "b = ",b);
+
+//Second type of Swap
+ 
+var x = 4;
+var y = 8;
+console.log("Before swap: x = ",x, "y = ",y);
+var x = x + y;
+var y = x - y;
+var x = x - y;
+console.log("After swap: x = ",x, "y = ",y);
+
+//Third type of Swap
+
+var p = 9;
+var q = 7;
+console.log("Before swap: p = ",p, "q = ",q);
+[p, q] = [q, p];
+console.log("After swap: p = ",p, "q = ",q);
