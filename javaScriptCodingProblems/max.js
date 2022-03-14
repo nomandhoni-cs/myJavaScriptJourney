71538f6a85e1ec6a263dc07cc9dbc714c1f0b508
nomandhoni-cs <nomandhoni@yahoo.com>
2022-03-14 15:47:42 +0600

    Renamed Variable and learned by pen paper

---
+++ javaScriptCodingProblems/max.js
@@ -0,0 +1,19 @@
+var noman = 150;
+var sabbir = 650;
+var mridul = 550;
+if(noman> sabbir){
+    if (noman > mridul){
+        console.log("noman is Bigger");
+    }
+    else{
+        console.log("mridul is Bigger");    
+    }
+}
+else{
+    if (sabbir > mridul) {
+    console.log("sabbir is Bigger");
+    }
+    else{
+    console.log("mridul is Bigger");
+    }
+}
