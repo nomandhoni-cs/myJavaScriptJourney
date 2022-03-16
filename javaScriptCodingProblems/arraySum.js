dc8c1031dbe87fedb7dab5876668e163602bca15
nomandhoni-cs <nomandhoni@yahoo.com>
2022-03-16 14:15:25 +0600

    Added array sum and debugged

---
+++ javaScriptCodingProblems/arraySum.js
@@ -0,0 +1,8 @@
+var numbers = [3, 4, 6, 29, 23];
+var sum = 0;
+for (let i = 0; i < numbers.length; i++) {
+    var element = numbers[i];
+    sum = sum + element;
+    
+}
+console.log("Some of all numbers: ",sum);
