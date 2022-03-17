fb4e47fecccde422619cdedcc89ca88b861cd6dd
nomandhoni-cs <nomandhoni@yahoo.com>
2022-03-17 17:52:09 +0600

    Added comment in duplicate finding array and debugged and understand completly

--- javaScriptCodingProblems/duplicate.js
+++ javaScriptCodingProblems/duplicate.js
@@ -2,8 +2,11 @@ var idNumber = [1, 3, 4, 2, 5, 3, 7, 3, 2];
 var uniqueId = [];
 for(let i = 0; i < idNumber.length; i++){
     var element = idNumber[i];
+    //Here below line will check is this number is in the unique number array.
+    //If not it will return the index value -1
     var index = uniqueId.indexOf(element);
     if(index == -1){
+        //.push to add element into array
         uniqueId.push(element);
     }
 }
