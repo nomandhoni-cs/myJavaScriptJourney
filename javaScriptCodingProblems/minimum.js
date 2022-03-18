3763818c4d22bf3583de298035237f7beab562eb
nomandhoni-cs <nomandhoni@yahoo.com>
2022-03-18 16:30:03 +0600

    Added a file called minimum.js to find minimum value

---
+++ javaScriptCodingProblems/minimum.js
@@ -0,0 +1,8 @@
+//By this program we can find minimum value.
+var marksOfSabbir = 49;
+var marksofNoman = 31;
+var marksOfDipu = 39;
+var marksOfMridul = 30;
+// Here we are going to find who got the minimum marks.
+var minimumMarks = Math.min(marksOfSabbir, marksOfMridul, marksOfDipu, marksofNoman);
+console.log(minimumMarks);
