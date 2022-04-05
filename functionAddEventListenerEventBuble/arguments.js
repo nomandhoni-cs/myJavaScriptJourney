1fa987f8d849d4c4542094809032f864c396641d
nomandhoni-cs <nomandhoni@yahoo.com>
2022-04-05 19:20:06 +0600

    Added arguments.js file and learned about using argumets is JavaScript

---
+++ functionAddEventListenerEventBuble/arguments.js
@@ -0,0 +1,15 @@
+//arguments is like an object. You can use it only inside of a function
+function addNumbers(numberOne, numberTwo) {
+  var sum = 0;
+  // sum = numberOne + numberTwo that means you can't sum more than two parameter
+  //By using arguments you can access all of the parameter
+  //arguments like an array but you can't push or pop element onto it.
+  console.log("This is the argument ", arguments);
+  for (let i = 0; i < arguments.length; i++) {
+    const element = arguments[i];
+    sum = sum + element;
+  }
+  return sum;
+}
+var result = addNumbers(1, 3, 5, 9);
+console.log(result);
