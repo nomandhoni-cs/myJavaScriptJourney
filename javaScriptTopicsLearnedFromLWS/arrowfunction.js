a2bb88c3a5e36eb27c4df983edf0fd2eede5061e
nomandhoni-cs <nomandhoni@yahoo.com>
2022-04-20 14:07:28 +0600

    Learned few things about arrow function

---
+++ javaScriptTopicsLearnedFromLWS/arrowfunction.js
@@ -0,0 +1,10 @@
+let arrow = () => 'Hello World By Arrow Function';
+console.log(arrow);
+let arrowConsole = () => {
+var a = 4;
+var b = 7;
+let c = a * b;
+console.log(c);
+}
+var result = arrowConsole();
+console.log(result);
